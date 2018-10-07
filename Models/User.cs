ccb7820eb5b4a58c58a7d36f4117795e5fe3d935
Michael Smith <msmithboise@gmail.com>
2018-10-07 00:49:12 -0600

    fixed

--- Models/User.cs
+++ Models/User.cs
@@ -6,83 +6,52 @@ using System.Security.Claims;
 namespace burgershack.Models
 {
 
-    public class UserLogin
-    { // HELPER MODEL
-      // this is the info I need to create a user, its to keep it private.
-      // this is your login form and registration form
-      // this is the info your client has to pass on the frontend.
-
+    public class UserLogin // HELPER MODEL
+    {
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-
         [Required]
         [MinLength(6)]
-        public string password { get; set; }
-
-
+        public string Password { get; set; }
     }
+    public class UserRegistration // HELPER MODEL
+    {
 
-    public class UserRegistration
-    { // HELPER MODEL
-      // this is the info I need to create a user, its to keep it private.
-      // this is your login form and registration form
-      // this is the info your client has to pass on the frontend.
+        [Required]
+        public string Username { get; set; }
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-
-
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
     }
 
     public class User
     {
-
         public string Id { get; set; }
-
         public bool Active { get; set; } = true;
-
-
         public string Username { get; set; }
-
         [Required]
-
-
         internal string Hash { get; set; }
-        // internal means the front end never recieves it.  
-        // Hash is to retrieve a user by their email.
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
         public ClaimsPrincipal _principal { get; private set; }
-        // this is your token.  Its the id that points to your user object.
+
+
 
         internal void SetClaims()
         {
-
-            // this is saying req.session.uid = id
             var claims = new List<Claim>{
-                new Claim(ClaimTypes.Email, Email),
-                new Claim(ClaimTypes.Name, Id)
-            };
+        new Claim(ClaimTypes.Email, Email),
+        new Claim(ClaimTypes.Name, Id) //req.session.uid = id
+      };
             var userIdentity = new ClaimsIdentity(claims, "login");
             _principal = new ClaimsPrincipal(userIdentity);
-
         }
-
-
-        // public User(){
-        //     Email = "J@j.com"
-        // };
-
-
-
-
-
-
     }
-
-
 }
